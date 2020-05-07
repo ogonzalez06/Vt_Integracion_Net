b9352e78152e6fd8a3c016ab2e6e0290f5e07ccd
Oscar <oscar06.gonzalez@gmail.com>
2020-05-07 13:34:31 -0400

    test1

---
+++ VidaSecurity/Proyecto_2/Controllers/PaisController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestWebApi.Models;
+
+namespace TestWebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/pais")]
+    [ApiController]
+    public class PaisController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext context;
+
+        public PaisController (ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Pais> Get()
+        {
+            return context.Paises.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var pais = context.Paises.FirstOrDefault(x => x.Id == id);
+            if (pais == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pais);
+        }
+
+    }
+}
---
+++ VidaSecurity/Proyecto_2/Startup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using TestWebApi.Models;
+
+namespace TestWebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                   options.UseInMemoryDatabase("pasiDB"));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDbContext context)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            //QA CC a Repos
+            if (!context.Paises.Any())
+            {
+                context.Paises.AddRange(new List<Pais>()
+                {
+                    new Pais(){Nombre = "República Dominicana"},
+                    new Pais(){Nombre = "México"},
+                    new Pais(){Nombre = "Argentina"}
+                });
+
+                context.SaveChanges();
+            }
+
+
+
+
+        }
+    }
+}
